febc34d9a316a6131247c9fe04e9ed6145ae0ef2
apple <apple@apples-MacBook-Pro-2.local>
2020-03-11 20:36:28 -0700

    set up post, tested working

--- data/router.js
+++ data/router.js
@@ -18,5 +18,54 @@ router.get('/', (req, res) => {
     });
   });
 
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Post not found' });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the Post',
+      });
+    });
+  });
+
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the hub',
+      });
+    });
+  });
+
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+    .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ message: 'Post not found' });
+        }
+      })
+      .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+          message: 'Error retrieving the Post',
+        });
+      });
+    });
 
 module.exports = router;
