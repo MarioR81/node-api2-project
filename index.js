4f4039e9683f4faa274709aa1a05ebefb65ad60a
apple <apple@apples-MacBook-Pro-2.local>
2020-03-11 18:26:28 -0700

    set up project

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const server = require('./api/server.js');
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)
+});
